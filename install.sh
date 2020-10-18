dfc862b59b3913ac7b1bbf9b6d7db8df4626fdcd
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2020-10-18 11:56:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # coded by cybereagle2001
 git clone https://github.com/Moham3dRiahi/Th3inspector.git
 git clone https://github.com/trustedsec/social-engineer-toolkit.git
-git clone https://github.com/sherlock-project/sherlock.git
+https://github.com/BullsEye0/shodan-eye.git
 git clone https://github.com/cybereagle2001/Crypto.git
 git clone https://github.com/Mebus/cupp.git
 git clone https://github.com/nmap/ncrack.git
